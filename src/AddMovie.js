9e666393c30d7c5251833eea2e8349a28c9960f4
Igor de Paula <principe.borodin@gmail.com>
2019-12-12 23:13:15 -0300

    add fix update object

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -4,22 +4,40 @@ import {MovieContext} from "./MovieContext";
 const AddMovie = () => {
     const [name, setName] = useState('')
     const [price, setPrice] = useState('')
+    const [Id, setId] = useState('')
     const [movies, setMovies] = useContext(MovieContext)
-    const updateName = e =>{
-         setName(e.target.value)
+    const updateName = e => {
+        setName(e.target.value)
     }
-    const updatePrice = e =>{
-         setPrice(e.target.value)
+    const updatePrice = e => {
+        setPrice(e.target.value)
+    }
+    const updateId = e => {
+        setId(e.target.value)
     }
 
     const AddMovie = e => {
-e.preventDefault();
-setMovies(prevMovies => [...prevMovies,{name:name, price: price, id: movies.length +1}])
+        e.preventDefault();
+        setMovies(prevMovies => {
+            let index = prevMovies.findIndex(i => i.id == Id)
+            if (index > -1) {
+                let movies = prevMovies.splice(index, 1, {name: name, price: price, id: Id})
+
+                return [...prevMovies]
+            } else {
+                return [
+                    ...
+                        prevMovies, {name: name, price: price, id: movies.length + 1}
+                ]
+            }
+
+        })
     }
 
 
     return (
         <form onSubmit={AddMovie}>
+            <input type="text" name={Id} value={Id} onChange={updateId}/><br/>
             <input type="text" name={name} value={name} onChange={updateName}/><br/>
             <input type="text" name={price} value={price} onChange={updatePrice}/><br/>
             <button>salvar</button>
